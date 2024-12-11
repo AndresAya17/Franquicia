0f7730ddcc50e676d6f04cb66439ee698b8a2fbf
AndresAya17 <38768365+AndresAya17@users.noreply.github.com>
2024-12-10 21:48:09 -0500

    Proyecto raiz V1

---
+++ src/main/java/com/nequi/ms_franquicias/MsFranquiciasApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.ms_franquicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFranquiciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFranquiciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nequi/ms_franquicias/MsFranquiciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.ms_franquicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFranquiciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
